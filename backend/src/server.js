c38d8779fcf8cf91c2c65bf99d3cddc1137474ca
zeusdarkly <yazilim887@gmail.com>
2025-09-21 13:14:17 +0300

    Update server.js
    Errors fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);k
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
